37e49bd36a8a1e17a260cc88f1a9147f1cec3388
noirvin <vin.sp97@googlemail.com>
2020-03-05 12:58:11 -0800

    hashtable almost done

--- hashtable.py
+++ hashtable.py
@@ -39,6 +39,11 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Collect all values in each bucket
+        all_vals = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_vals.append(value)
+        return all_vals
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
@@ -54,28 +59,58 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Count number of key-value entries in each bucket
+        sum = 0
+        for bucket in self.buckets:
+            for key in bucket.keys():
+
+                sum+=1
+        return sum
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Find bucket where given key belongs
+        bucket_loc = self._bucket_index(key)
+        bucket = self.buckets[bucket_loc]
         # TODO: Check if key-value entry exists in bucket
+        if key in bucket.keys():
+            return True
+        else:
+            return False
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError.
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Find bucket where given key belongs
+        bucket_loc = self._bucket_index(key)
+        bucket = self.buckets[bucket_loc]
+        target_node = bucket.find(lambda item: item [0] == key)
+
         # TODO: Check if key-value entry exists in bucket
+        if target_node != None:
+            return target_node[1]
         # TODO: If found, return value associated with given key
         # TODO: Otherwise, raise error to tell user get failed
-        # Hint: raise KeyError('Key not found: {}'.format(key))
+        else:
+
+            KeyError('Key not found: {}'.format(key))
 
     def set(self, key, value):
         """Insert or update the given key with its associated value.
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Find bucket where given key belongs
+        bucket_loc = self._bucket_index(key)
+        bucket = self.buckets[bucket_loc]
+        target_node = bucket.find(lambda item: item [0] == key)
+
         # TODO: Check if key-value entry exists in bucket
+        if target_node!=None:
+            bucket.delete(target_node)
+            bucket.append((key,value))
+
         # TODO: If found, update value associated with given key
+        else:
+            bucket.append((key,value))
         # TODO: Otherwise, insert given key-value entry into bucket
 
     def delete(self, key):
